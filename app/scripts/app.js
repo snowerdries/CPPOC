b5f57c03071f7042cad8ce756c94db1e28826859
snowerdries <dries.nicolay@gmail.com>
2014-08-01 09:22:11 +0200

    ContactView

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 /*jshint unused: vars */
-define(['angular', 'controllers/main', 'controllers/about']/*deps*/, function (angular, MainCtrl, AboutCtrl)/*invoke*/ {
+define(['angular', 'controllers/main', 'controllers/about', 'controllers/contact']/*deps*/, function (angular, MainCtrl, AboutCtrl, ContactCtrl)/*invoke*/ {
   'use strict';
 
   /**
@@ -13,6 +13,7 @@ define(['angular', 'controllers/main', 'controllers/about']/*deps*/, function (a
   return angular
     .module('cppocApp', ['cppocApp.controllers.MainCtrl',
 'cppocApp.controllers.AboutCtrl',
+'cppocApp.controllers.ContactCtrl',
 /*angJSDeps*/
     'ngResource',
     'ngRoute'
@@ -28,6 +29,10 @@ define(['angular', 'controllers/main', 'controllers/about']/*deps*/, function (a
         .when('/about', {
           templateUrl: 'views/about.html',
           controller: 'AboutCtrl'
+        })
+        .when('/contact', {
+          templateUrl: 'views/contact.html',
+          controller: 'ContactCtrl'
         })
         .otherwise({
           redirectTo: '/'
